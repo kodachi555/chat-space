104794b50caf236a38dbf6f402dbe9ced37c51a7
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-03-13 20:43:54 +0900

    add-user-edit-with-devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/edit'
 
-  get 'users/update'
-
+  # resourceと似たようなもん。deviseの便利機能
   devise_for :users
   root 'messages#index'
+  # users/:id/edit　と　users/:id/update　にルートを設定
+  resource :users, only: [:edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
