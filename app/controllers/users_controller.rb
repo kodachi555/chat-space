eab26131b63cd6024bbd2e27ea67279d70e3c426
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-03-14 14:27:16 +0900

    Before-add-notification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     # ストロングパラメータが通ったら
     if current_user.update(user_params)
       # rootにリダイレクト
+      flash[:notice]="ユーザ情報を更新しました"
       redirect_to root_path
     else
       # 通らなかったらeditに戻る
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   # users/:id/edit　と　users/:id/update　にルートを設定
-  resource :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
