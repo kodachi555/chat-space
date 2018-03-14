1f9d455c4c83819b542207cbd8a877b974e4287f
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-03-14 18:53:18 +0900

    add-routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root 'messages#index'
   # users/:id/edit　と　users/:id/update　にルートを設定
   resources :users, only: [:edit, :update]
-  resources :groups
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
