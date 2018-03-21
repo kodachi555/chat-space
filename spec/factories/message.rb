d67af148fe40ea1309375ec6151ac909b3c75dc0
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-03-21 17:46:16 +0900

    test-with-devise

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :message do
     content Faker::Lorem.sentence
     image File.open("#{Rails.root}/public/images/no_image.jpg")
+    # image
     user
     group
   end
