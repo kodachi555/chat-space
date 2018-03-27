c939f087cabeae77316facd6a5e152d1459566eb
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-03-27 21:58:00 +0900

    correction-filter-current-user
    WHAT
    current_userを渡さないモデルの書き方を変えました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id:current_user.id)
+    @users = User.where('name LIKE(?) AND id != (?)', "%#{params[:keyword]}%",current_user.id)
     respond_to do |format|
       format.json
     end
