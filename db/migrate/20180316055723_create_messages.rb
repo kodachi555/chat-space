91e0b52fdbf29f260e49586eb0a06dc20500e38f
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-04-02 15:19:35 +0900

    for-develop

--- db/migrate/20180316055723_create_messages.rb
+++ db/migrate/20180316055723_create_messages.rb
@@ -4,7 +4,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
       t.string :body
       t.string :image
       t.references :group, foreign_key: true, index: true
-      t.references :user, foreign_key: true, index: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
