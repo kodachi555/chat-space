146ad978e833797281cc7e34017766692181ec91
Nobuyuki Miyake <kodachi555@yahoo.co.jp>
2018-04-02 15:49:24 +0900

    set-key-for-deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,7 @@ set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['Users/n.m/.ssh/ec2-user.pem']
-                  
+
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
@@ -62,3 +62,7 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 end
+
+set :default_env, {
+  :DEVISE_SECRET_KEY => ENV['DEVISE_SECRET_KEY']
+}
